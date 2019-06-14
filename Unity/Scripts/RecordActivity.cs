def388940c3b403a9dd5d39a7856d2f2a91c0464
rashwinr <ark440@nyu.edu>
2019-06-14 17:54:02 -0400

    Satish commit 5c3e17b pasted with 28b8cfa

--- Unity/Scripts/RecordActivity.cs
+++ Unity/Scripts/RecordActivity.cs
@@ -2,32 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 
 public class RecordActivity : MonoBehaviour
 {
-    public Dictionary<string,List<Vector3[]>> Activity;
+    public Dictionary<string, List<string>> Activity;
     public InputField ActivityName;
-    public List<Vector3[]> ActivityKeys;
-    public Vector3[] Angles;
+    public List<string> ActivityKeys;
+    public Quaternion[] Angles;
     private Connection Conn;
     public float TimeTaken;
     public int KeyNo;
     public Transform[] CameraTransforms;
     public GameObject cam;
     public Dropdown CameraView;
-    
+    public List<GameObject> ClonedObjects = new List<GameObject>();
+
+    private static string savedActivityPath;
+    private static string savedDataPath;
+
+    public bool ver_log;
     // Start is called before the first frame update
     void Start()
     {
+        savedActivityPath = Application.persistentDataPath + "/ActivityData";
+        savedDataPath = Application.persistentDataPath + "/savedData";
         Conn = GetComponent<Connection>();
-        Angles = new Vector3[4];
-        ActivityKeys = new List<Vector3[]>();
-        Activity = new Dictionary<string, List<Vector3[]>>();
+        Angles = new Quaternion[Conn.no_devices];
+        ActivityKeys = new List<string>();
+        Activity = new Dictionary<string, List<string>>();
         KeyNo = 0;
         //setting initial camera position
         cam.transform.position = CameraTransforms[0].position;
         cam.transform.rotation = CameraTransforms[0].rotation;
+        GetAllRecordedData();
     }
+
+    #region Recording Activities
+    
 
     public void AddActivity()
     {
@@ -36,41 +48,29 @@ public class RecordActivity : MonoBehaviour
             ActivityName.text = "Untitled_Activity";
         }
 
-
+        string Path = savedActivityPath + "/" + ActivityName.text;
         Activity.Add(ActivityName.text, ActivityKeys);
+        string Data = "";
+        foreach(var Key in ActivityKeys)
+        {
+            Data += Key + "\n";
+        }
+        SaveActivity(Path,Data);
         PrintAllActivities();
         KeyNo = 0;
     }
 
     public void ChangeCameraView()
     {
-        switch (CameraView.value)
-        {
-            case 0:
-                cam.transform.position = CameraTransforms[0].position;
-                cam.transform.rotation = CameraTransforms[0].rotation;
-                break;
-            case 1:
-                cam.transform.position = CameraTransforms[1].position;
-                cam.transform.rotation = CameraTransforms[1].rotation;
-                break;
-            case 2:
-                cam.transform.position = CameraTransforms[2].position;
-                cam.transform.rotation = CameraTransforms[2].rotation;
-                break;
-        }
+        cam.transform.position = CameraTransforms[CameraView.value].position;
+        cam.transform.rotation = CameraTransforms[CameraView.value].rotation;
     }
 
     public void AddKey()
-    { 
-        Angles[0] = new Vector3(Conn.angle_x[0], Conn.angle_y[0], Conn.angle_z[0]);
-        Angles[1] = new Vector3(Conn.angle_x[1], Conn.angle_y[1], Conn.angle_z[1]);
-        Angles[2] = new Vector3(Conn.angle_x[2], Conn.angle_y[2], Conn.angle_z[2]);
-        Angles[3] = new Vector3(Conn.angle_x[3], Conn.angle_y[3], Conn.angle_z[3]);
-
-        ActivityKeys.Add(Angles);
+    {        
+        ActivityKeys.Add(Conn.DeviceLocalAngles);
         KeyNo = KeyNo + 1;
-       PrintAllActivityElements(ActivityKeys);
+        PrintAllActivityElements(ActivityKeys);
     }
 
     public void RemoveKey()
@@ -88,19 +88,243 @@ public class RecordActivity : MonoBehaviour
         }
     }
 
-    public void PrintAllActivityElements(List<Vector3[]> AKeys)
+    public void PrintAllActivityElements(List<string> AKeys)
     {
         foreach(var item in AKeys)
         {
-            foreach(var Vec in item)
-            {
-                Debug.Log(Vec);
-            }
+            //foreach(var Vec in item)
+            //{
+                Debug.Log(item);
+            //}
         }
     }
+
+    #endregion
 
     public void PlayActiviy()
     {
 
     }
+
+    #region Saving Activity
+    void SaveActivity(string Path, string Data)
+    {
+        string DictionaryPath = Path + ".txt";
+        if (!File.Exists(DictionaryPath))
+        {
+            Directory.CreateDirectory(savedActivityPath);
+        }
+        File.WriteAllText(DictionaryPath, Data);
+    }
+    #endregion
+
+    #region Displaying Subject Recordings and Activities
+
+    public RectTransform Content;
+    public GameObject Item_Prefab;
+    public Dropdown Activities;
+    public Dropdown Subjects;
+    public List<Vector2> ContentSize = new List<Vector2>();
+    public List<GameObject> SubjectsUI = new List<GameObject>();
+
+    public string[] ActivityFileNames;//Names of Activities
+    public string[] SubjectIDs;//Names of Directories
+
+    void GetActivities()
+    {
+        if (Directory.Exists(savedActivityPath))
+        {
+            string[] fileInfo = Directory.GetFiles(savedActivityPath, "*.txt");
+
+            for (int i = 0; i < fileInfo.Length; i++)
+            {
+                fileInfo[i] = Path.GetFileName(fileInfo[i]);
+                if (fileInfo[i] != "")
+                {
+                    string[] SplitNames = fileInfo[i].Split('.');
+                    fileInfo[i] = SplitNames[0];
+                }
+
+                if (ver_log)
+                {
+                    Debug.Log(Path.GetFileName(fileInfo[i]));
+                }
+            }
+            ActivityFileNames = fileInfo;
+        }
+    }
+
+    void GetSubjects()
+    {
+        if (Directory.Exists(savedDataPath))
+        {
+            string[] DirectoryInfo = Directory.GetDirectories(savedDataPath);
+            for (int i = 0; i < DirectoryInfo.Length; i++)
+            {
+                DirectoryInfo[i] = Path.GetFileName(DirectoryInfo[i]);
+                if (ver_log)
+                {
+                    Debug.Log(DirectoryInfo[i]);
+                }
+            }
+            SubjectIDs = DirectoryInfo;
+        }
+    }
+
+    string[] GetSubjectActivities(string SubjectID)
+    {
+        
+        string[] DirectoryInfo = new string[1];
+        if (Directory.Exists(savedDataPath + "/" + SubjectID))
+        {
+            DirectoryInfo = Directory.GetDirectories(savedDataPath + "/" + SubjectID);
+            for (int i = 0; i < DirectoryInfo.Length; i++)
+            {
+                DirectoryInfo[i] = Path.GetFileName(DirectoryInfo[i]);
+                if (ver_log)
+                {
+                    Debug.Log(DirectoryInfo[i]);
+                }
+            }
+        }
+        return DirectoryInfo;
+    }
+
+    string[] GetSubjectRecordingTimeStamps(string ActivityName, string SubjectID)
+    {
+        string[] fileInfo = new string[1];
+        
+        if (Directory.Exists(savedDataPath + "/" + SubjectID + "/" + ActivityName))
+        {
+            fileInfo = Directory.GetFiles(savedDataPath + "/" + SubjectID + "/" + ActivityName, "*.txt");
+            string[] filePaths = new string[fileInfo.Length*2];
+            for (int i = 0; i < fileInfo.Length; i++)
+            {
+                filePaths[i + fileInfo.Length] = fileInfo[i];
+                fileInfo[i] = Path.GetFileName(fileInfo[i]);
+                if (fileInfo[i] != "")
+                {
+                    string[] SplitNames = fileInfo[i].Split('_');
+                    string[] TimeStamp = new string[6];
+
+                    for(int j = SplitNames.Length - 6; j < SplitNames.Length; j++)
+                    {
+                       // Debug.Log(i + SplitNames[i] + SplitNames.Length);
+                        TimeStamp[j + 6 - SplitNames.Length] = SplitNames[j];
+                        if((j + 6 - SplitNames.Length) == 5)
+                        {
+                            string[] SplitText = TimeStamp[j + 6 - SplitNames.Length].Split('.');
+                            TimeStamp[j + 6 - SplitNames.Length] = SplitText[0];
+                        }
+                    }
+                    
+                    fileInfo[i] = TimeStamp[1] + "/" + TimeStamp[0] + "/" + TimeStamp[2] + "  " + TimeStamp[3] + ":" + TimeStamp[4] + ":" + TimeStamp[5];
+                    //Debug.Log("Done");
+                }
+
+                if (ver_log)
+                {
+                    Debug.Log(fileInfo[i]);
+                }
+                //Debug.Log(i + "," + fileInfo.Length + "," + filePaths.Length);
+                filePaths[i] = fileInfo[i];
+                //Debug.Log(filePaths[i + fileInfo.Length]);
+            }
+            return filePaths;
+        }
+        return fileInfo;
+    }
+
+    public GameObject EmptyObj;
+
+    void GetAllRecordedData()//Should be called only once at the start of the program
+    {
+        Activities.ClearOptions();
+        Subjects.ClearOptions();
+        ContentSize.Clear();
+        GetActivities();
+        GetSubjects();
+        SubjectsUI.Clear();
+        List<string> Sub_Act = new List<string>();
+        Sub_Act.AddRange(SubjectIDs);
+        Subjects.AddOptions(Sub_Act);
+        Sub_Act.Clear();
+
+        Sub_Act.AddRange(ActivityFileNames);
+        Activities.AddOptions(Sub_Act);
+        Subjects.AddOptions(Sub_Act);
+
+        foreach (string SubjectID in SubjectIDs)
+        {
+            GameObject EmptySubject = Instantiate(EmptyObj, Content);
+            EmptySubject.name = SubjectID;
+            ClonedObjects.Add(EmptySubject);
+            string[] SubjectActivities;//Names of Directories of Activitiesz    
+            SubjectActivities = GetSubjectActivities(SubjectID);
+            float TempSize = 0f;
+            foreach (string Activity in SubjectActivities)
+            {
+                string[] SubjectRecordingTimeStamps;//Names of Recording Files Time Stamps of Subject Activities
+                SubjectRecordingTimeStamps = GetSubjectRecordingTimeStamps(Activity, SubjectID);
+                
+                for (int i = 0; i < SubjectRecordingTimeStamps.Length/2; i++)
+                {
+                    //Debug.Log("Done");
+
+                    
+                    GameObject Clone = Instantiate(Item_Prefab, EmptySubject.transform);
+                    ClonedObjects.Add(Clone);
+                    RectTransform CloneTransform = Clone.GetComponent<RectTransform>();
+                    CloneTransform.localPosition = new Vector3(-6.1f, (-42.0f - (110.0f * i))-TempSize, 0f);
+                    SetupAndLoadData CloneSLD = Clone.GetComponent<SetupAndLoadData>();
+                    CloneSLD.TimeStamp = SubjectRecordingTimeStamps[i];
+                    CloneSLD.ActivityName = Activity;
+                    CloneSLD.Path = SubjectRecordingTimeStamps[i + (SubjectRecordingTimeStamps.Length/2)];
+                    CloneSLD.LoadingTextReferenceDataDone();
+                }
+                TempSize += (SubjectRecordingTimeStamps.Length / 2) * 110.0f;
+            }
+            if (TempSize > 580.0f)
+            {
+                ContentSize.Add(new Vector2(Content.sizeDelta.x, TempSize));
+            }
+            else
+            {
+                ContentSize.Add(new Vector2(Content.sizeDelta.x, 580.0f));
+            }
+            SubjectsUI.Add(EmptySubject);
+            DisableAllContent();
+        }
+    }
+
+    public void RefreshFiles()
+    {
+        DeleteAllClonedObjects();
+        GetAllRecordedData();
+    }
+
+    private void DeleteAllClonedObjects()
+    {
+        foreach(GameObject Clone in ClonedObjects)
+        {
+            Destroy(Clone);
+        }
+        ClonedObjects.Clear();
+    }
+
+    public void SetContentSize()
+    {
+        DisableAllContent();
+        Content.sizeDelta = ContentSize[Subjects.value];
+        SubjectsUI[Subjects.value].SetActive(true);
+    }
+
+    public void DisableAllContent()
+    {
+        foreach(GameObject Subject in SubjectsUI)
+        {
+            Subject.SetActive(false);
+        }
+    }
+    #endregion
 }
---
+++ matlab/functions_jack_and_codes/kinect_imu_comparision_final_14_6.m
@@ -0,0 +1,267 @@
+%% Initialization section
+delete(instrfind({'Port'},{'COM15'}))
+clear all; close all;clc;
+markers = ["lef","lbd","lelb","lelb1","lps","lie","lie1","ref","rbd","relb","relb1","rps","rie","rie1"];
+
+SUBJECTID = 1234;
+
+flg = 1;
+
+if flg 
+    Offsets = [0.8589,0.0411,-0.0170,-0.5101; 1.0000,0,0,0; -0.8954,0.0070,0.0010,0.4452; -0.9749,0.0070,-0.0250,0.2210; 0.9791,-0.0371,-0.0170,-0.1994];
+end
+       
+%Kinect initialization script
+addpath('F:\github\wearable-jacket\matlab\KInectProject\Kin2');
+addpath('F:\github\wearable-jacket\matlab\KInectProject\Kin2\Mex');
+addpath('F:\github\wearable-jacket\matlab\KInectProject');
+k2 = Kin2('color','depth','body','face');
+outOfRange = 4000;c_width = 1920; c_height = 1080;COL_SCALE = 1.0;
+color = zeros(c_height*COL_SCALE,c_width*COL_SCALE,3,'uint8');
+c.h = figure(1);c.ax = axes;c.im = imshow(color,[]);
+set( gcf, 'DoubleBuffer', 'on','keypress','k=get(gcf,''currentchar'');' );
+%quaternion variables
+qC = [1,0,0,0];qD = [1,0,0,0];qA = [1,0,0,0];qB = [1,0,0,0];qE = [1,0,0,0];empty = [1,0,0,0];
+Cal_A = [0 0 0 0];Cal_B = [0 0 0 0];Cal_C = [0 0 0 0];Cal_D = [0 0 0 0];Cal_E = [0 0 0 0];
+limuef = 0;rimuef = 0;lkinef = 0;rkinef = 0;
+limubd = 0;rimubd = 0;lkinbdangle = 0;rkinbd = 0;
+limuie = 0;rimuie = 10;lkinie = 0;rkinie = 0;
+limuelb = 0;rimuelb = 0;lkinelb = 0;rkinelb = 0;
+limuelb1 = 0;rimuelb1 = 0;lkinelb1 = 0;rkinelb1 = 0;
+xlimuef = 0;xrimuef = 0;xlkinef = 0;xrkinef = 0;
+xlimubd = 0;xrimubd = 0;xlkinbd = 0;xrkinbd = 0;
+xlimuie = 0;xrimuie = 10;xlkinie = 0;xrkinie = 0;
+xlimuelb = 0;xrimuelb = 0;xlkinelb = 0;xrkinelb = 0;
+xlimuelb1 = 0;xrimuelb1 = 0;xlkinelb1 = 0;xrkinelb1 = 0;
+
+ls = 0;rs = 1350;lw = 475;H = 1080;rw = 570;     %rectangle coordinates
+%COM Port details
+delete(instrfind({'Port'},{'COM15'}))
+ser = serial('COM15','BaudRate',115200,'InputBufferSize',100);
+ser.ReadAsyncMode = 'continuous';
+fopen(ser);k=[];
+
+ %% IMU offsetdetermination
+while ~flg
+    
+    flushinput(ser);
+    line = fscanf(ser);   % get data if there exists data in the next line
+    data = strsplit(string(line),',');
+    if(length(data) == 5 || length(data) == 6)
+    switch data(1)
+        case 'cal'
+          switch data(2)
+                case 'b'
+                    B_mag = str2double(data(3));B_acc = str2double(data(4));B_gyr = str2double(data(5));B_sys = str2double(data(6));
+                    Cal_B = [B_mag B_acc B_gyr B_sys];
+                case 'a'
+                    A_mag = str2double(data(3));A_acc = str2double(data(4));A_gyr = str2double(data(5));A_sys = str2double(data(6));      
+                    Cal_A = [A_mag A_acc A_gyr A_sys];
+                case 'c'
+                    C_mag = str2double(data(3));C_acc = str2double(data(4));C_gyr = str2double(data(5));C_sys = str2double(data(6));  
+                    Cal_C = [C_mag C_acc C_gyr C_sys];
+                case 'd'
+                    D_mag = str2double(data(3));D_acc = str2double(data(4));D_gyr = str2double(data(5));D_sys = str2double(data(6));      
+                    Cal_D = [D_mag D_acc D_gyr D_sys];
+                case 'e'
+                    E_mag = str2double(data(3));E_acc = str2double(data(4));E_gyr = str2double(data(5));E_sys = str2double(data(6));      
+                    Cal_E = [E_mag E_acc E_gyr E_sys];
+          end 
+                case 'e'
+            qE = qconvert(data);
+            if flg
+                break
+            else 
+                [flg,Offsets] = find_offsets(qA,qB,qC,qD,qE);
+            end
+                case 'a'
+           qA = qconvert(data);
+           if flg
+               break
+           else 
+                [flg,Offsets] = find_offsets(qA,qB,qC,qD,qE);
+           end
+                case 'c'
+           qC = qconvert(data);
+           if flg
+               break
+           else 
+                [flg,Offsets] = find_offsets(qA,qB,qC,qD,qE);
+           end
+                case 'd'
+           qD = qconvert(data);
+           if flg
+               break
+           else 
+                [flg,Offsets] = find_offsets(qA,qB,qC,qD,qE);
+           end
+                case 'b'
+            qB = qconvert(data);
+            if flg
+               break
+            else 
+                [flg,Offsets] = find_offsets(qA,qB,qC,qD,qE);
+            end
+    end 
+    end
+end
+
+%%  Complete routine for updating data with 14 different angles
+for i=1:14
+arg = char(markers(i));    
+[anline,anline1,fid] = TitleUpdate(arg,SUBJECTID);
+lc=1;l=0;lflag = 0;telapsed=0;
+while (lc) 
+   tstart = tic;
+   if ser.BytesAvailable
+       [qA,qB,qC,qD,qE] = DataReceive(ser,qA,qB,qC,qD,qE);
+       qE = fix_imu('e',qE,Offsets);
+       qA = fix_imu('a',qA,Offsets);
+       qC = fix_imu('c',qC,Offsets);
+       qD = fix_imu('d',qD,Offsets);
+       qB = fix_imu('b',qB,Offsets); 
+       lshoangle = getleftarm(qE,qC);
+       limuie = lshoangle(3);limubd = lshoangle(2);limuef = lshoangle(1); 
+       rshoangle = getrightarm(qE,qD);
+       rimuie = rshoangle(3);rimubd = rshoangle(2);rimuef = rshoangle(1); 
+       lwriangle = getleftwrist(qC,qA);limuelb = lwriangle(1);limuelb1 = lwriangle(2);
+       lshoangle = getleftarm(qE,qC);lwriangle = getleftwrist(qC,qA);
+       limuie = lshoangle(3);limubd = lshoangle(2);limuef = lshoangle(1); 
+       limuelb = lwriangle(1);limuelb1 = lwriangle(2);
+       rshoangle = getrightarm(qE,qD);rwriangle = getrightwrist(qD,qB);
+       rimuie = rshoangle(3);rimubd = rshoangle(2);rimuef = rshoangle(1);
+       rimuelb = rwriangle(1);rimuelb1 = rwriangle(2);
+       rwriangle = getleftwrist(qD,qB);
+       rimuelb = rwriangle(1);rimuelb1 = rwriangle(2);     
+   end
+   validData = k2.updateData;
+   if validData
+       depth = k2.getDepth;color = k2.getColor;face = k2.getFaces;
+       depth8u = uint8(depth*(255/outOfRange));depth8uc3 = repmat(depth8u,[1 1 3]);
+       figure(1)
+       color = imresize(color,COL_SCALE);c.im = imshow(color, 'Parent', c.ax);
+       rectangle('Position',[0 0 475 1080],'LineWidth',3,'FaceColor','k');  
+       rectangle('Position',[1350 0 620 1080],'LineWidth',3,'FaceColor','k');
+       [bodies, fcp, timeStamp] = k2.getBodies('Quat');
+       numBodies = size(bodies,2);
+       if numBodies == 1
+           pos2Dxxx = bodies(1).Position; 
+           [lkinef,rkinef,lkinbdangle,rkinbd,lkinie,rkinie,lkinelb,rkinelb] = get_Kinect(pos2Dxxx);
+           k2.drawBodies(c.ax,bodies,'color',3,2,1);k2.drawFaces(c.ax,face,5,false,20);
+           switch arg
+                case 'lef'
+                    kin = lkinef; imu = limuef;
+                    lim = kin;
+                    tlow = 10; thigh=150;
+                case 'lbd'
+                    kin = lkinbdangle; imu = limubd;
+                    lim = kin;
+                    tlow = 20; thigh=150;
+                case 'lelb'
+                    kin = lkinelb; imu = limuelb;
+                    lim = kin;
+                    tlow = 20; thigh=130;
+                case 'lelb1'
+                    kin = lkinelb; imu = limuelb;
+                    lim = kin;
+                    tlow = 20; thigh=130;
+                case 'lps'
+                    kin = lkinelb1; imu = limuelb1;
+                    lim = imu;
+                    tlow = -45; thigh=45;
+                case 'lie'
+                    kin = lkinie; imu = limuie;
+                    lim = imu;
+                    tlow = -40; thigh=40;
+                case 'lie1' 
+                    kin = lkinie; imu = limuie;
+                    lim = imu;
+                    tlow = -40; thigh=40;
+                case 'ref'
+                    kin = rkinef; imu = rimuef;
+                    lim = kin;
+                    tlow = 10; thigh=150;
+                case 'rbd'
+                    kin = rkinbd; imu = rimubd;
+                    lim = kin;
+                    tlow = 20; thigh=150;
+                case 'relb'
+                    kin = rkinelb; imu = rimuelb;
+                    lim = kin;
+                    tlow = 20; thigh=130;
+                case 'relb1'
+                    kin = rkinelb; imu = rimuelb;
+                    lim = kin;
+                    tlow = 20; thigh=130;
+                case 'rps'
+                    kin = rkinelb1; imu = rimuelb1;
+                    lim = imu;
+                    tlow = -45; thigh=45;
+                case 'rie'
+                    kin = rkinelb1; imu = rimuelb1;
+                    lim = imu;
+                    tlow = -40; thigh=40;
+                case  'rie1'
+                    kin = rkinie; imu = rimuie;
+                    lim = imu;
+                    tlow = -40; thigh=40;
+           end
+           updateWiseKinect(arg,kin,imu,telapsed,anline,anline1)
+           %'Timestamp','Kinect_LeftShoulder_Ext.-Flex.','IMU_LeftShoulder_Ext.-Flex.','Kinect_LeftShoulder_Abd.-Add.','IMU_
+           % LeftShoulder_Abd.-Add.','Kinect_LeftShoulder_Int.-Ext.','IMU_LeftShoulder_Int.-Ext.','Kinect_LeftElbow_Ext.-Flex.','IMU_LeftElbow_Ext.-Flex.',
+           % 'IMU_LeftElbow_Pro.-Sup.','Kinect_RightShoulder_Ext.-Flex.','IMU_RightShoulder_Ext.-Flex.','Kinect_RightShoulder_Abd.-Add.','IMU_RightShoulder_Abd.-Add.',
+           %'Kinect_RightShoulder_Int.-Ext.','IMU_RightShoulder_Int.-Ext.','Kinect_RightElbow_Ext.-Flex.','IMU_RightElbow_Ext.-Flex.','IMU_RightElbow_Pro.-Sup.');
+           fprintf( fid, '%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f\n',telapsed,...
+           lkinef,limuef,lkinbdangle,limubd,lkinie,limuie,lkinelb,limuelb,limuelb1,rkinef,rimuef,...
+           rkinbd,rimubd,rkinie,rimuie,rkinelb,rimuelb,rimuelb1);
+       
+           if lim<=tlow
+              lflag = 1;
+           end
+           if (lim>=thigh) && lflag
+               l=l+1;
+               lflag =0;
+               if l>=8
+                   lc = 0;
+                   [~,~] = system('taskkill /F /IM Video.UI.exe');
+                   break;
+               end
+           end
+       end
+
+       if numBodies == 0
+           figure(1)
+           s1 = strcat('No persons in view');   
+           text((1920/2) - 250,100,s1,'Color','red','FontSize',30,'FontWeight','bold');
+       end      
+       if numBodies > 1
+           figure(1)
+           s1 = strcat('Too many people in view');
+           text(1920/2,100,s1,'Color','red','FontSize',30,'FontWeight','bold');
+       end      
+       if ~isempty(k)
+           if strcmp(k,'q') 
+           k=[];
+           break; 
+           end
+       end
+   end
+ pause(0.001);
+
+ if telapsed>=60
+     break;
+ end
+
+telapsed = telapsed+toc(tstart);
+end
+disp(telapsed);
+fclose(fid);
+
+clf(figure(2),'reset')
+end
+%% Closing everything 
+fclose(ser)
+delete(ser)
+close all;clear all;
+delete(instrfind({'Port'},{'COM15'}))
+

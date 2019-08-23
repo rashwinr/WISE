ad48f2327e1a77537c9ad172d3fbe804c19a0600
rashwinr <ark440@nyu.edu>
2019-08-23 09:53:54 -0400

    Repeatability and accuracy for IEEEsensors

--- matlab/IEEE_sensors/JCS_dataanalysis.m
+++ matlab/IEEE_sensors/JCS_dataanalysis.m
@@ -1,9 +1,96 @@
 clc;clear all;close all
-addpath('C:\Users\fabio\github\wearable-jacket\matlab\IEEE_sensors\');
-cd(strcat('C:\Users\fabio\github\wearable-jacket\matlab\IEEE_sensors\JCS_data\Male\'));
+addpath('F:\github\wearable-jacket\matlab\IEEE_sensors\');
+cd(strcat('F:\github\wearable-jacket\matlab\IEEE_sensors\JCS_data\Male\'));
 list = dir();
 
-Data = importJCS('Male_WISE+JCS_08-11-2019 19-39.txt');
+Data = importJCS('Male_WISE+JCS_08-23-2019 06-13.txt');
+
+%% plot all
+% Row = Data(:,10)>=200;
+% Data(Row,:) =[]; 
+Lstart = find(round(Data(:,1))<=3);
+Lstart = max(Lstart);
+Lend = find(Data(:,1)<=60);
+Lend = max(Lend);
+Rstart = find(Data(:,1)<=3);
+Rstart = max(Rstart);
+Rend = find(Data(:,1)<=60);
+Rend = max(Rend);
+% Lstart = 1;
+% Lend = length(Data);
+% Rstart = 1;
+% Rend = length(Data);
+LData = Data(Lstart:Lend,2:7);
+LTime = Data(1:length(LData),1);
+% Row = LData(:,1>=
+RData = Data(Rstart:Rend,8:end);
+RTime = Data(1:length(RData),1);
+LW = 2;
+
+figure(1)
+hold on
+
+% subplot(2,1,1)
+% hold on
+xlim([LTime(1),LTime(end)])
+% xTick('FontSize',15)
+% yTick('FontSize',15)
+Pl = plot(LTime,LData(:,1),'Color','r','LineWidth',LW,'DisplayName','Left Shoulder plane');
+El = plot(LTime,LData(:,2),'Color','g','LineWidth',LW,'DisplayName','Left Shoulder elevation');
+% Il =plot(LTime,LData(:,3),'Color','b','LineWidth',LW,'DisplayName','Left Shoulder Int.-Ext. rotation');
+Pr = plot(RTime,RData(:,1),'Color','r','LineWidth',LW,'LineStyle',':','DisplayName','Right Shoulder plane');
+Er = plot(RTime,RData(:,2),'Color','g','LineWidth',LW,'LineStyle',':','DisplayName','Right Shoulder elevation');
+% Ir = plot(RTime,RData(:,3),'Color','b','LineWidth',LW,'LineStyle',':','DisplayName','Right Shoulder Int.-Ext. rotation');
+xlabel('Time [s]','FontSize',15)
+ylabel('Angle [deg^o]','FontSize',15)
+lgd = legend([Pl,El,Pr,Er],'FontSize',12)
+lgd.Orientation = 'vertical'
+% 
+figure(2)
+% hold on
+% subplot(2,1,2)
+hold on
+xlim([LTime(1),LTime(end)])
+Fl = plot(LTime,LData(:,4),'Color','m','LineWidth',LW,'DisplayName','Left Elbow flex.-Ext.');
+Sl = plot(LTime,LData(:,6),'Color','k','LineWidth',LW,'DisplayName','Left Forearm Pro.-Sup.');
+Fr = plot(RTime,RData(:,4),'Color','m','LineStyle',':','LineWidth',LW,'DisplayName','Right Elbow flex.-Ext.');
+Sr = plot(RTime,RData(:,6),'Color','k','LineStyle',':','LineWidth',LW,'DisplayName','Right Forearm Pro.-Sup.');
+xlabel('Time [s]','FontSize',15)
+ylabel('Angle [deg^o]','FontSize',15)
+lgd1 = legend([Fl,Sl,Fr,Sr],'FontSize',12)
+lgd1.Orientation = 'vertical'
+
+
+
+% figure(2)
+% hold on
+% T2 = sgtitle('Right arm abduction-adduction');
+% 
+% subplot(2,1,1)
+% hold on
+% xlim([RTime(1),RTime(end)])
+% plot(RTime,RData(:,1),'Color','r','LineWidth',LW,'DisplayName','Shoulder plane');
+% plot(RTime,RData(:,2),'Color','g','LineWidth',LW,'DisplayName','Shoulder elevation');
+% plot(RTime,RData(:,3),'Color','b','LineWidth',LW,'DisplayName','Shoulder elevation');
+% xlabel('Time [s]')
+% ylabel('Angle [deg^o]')
+% 
+% subplot(2,1,2)
+% hold on
+% xlim([RTime(1),LTime(end)])
+% plot(RTime,RData(:,4),'Color','r','LineWidth',LW,'DisplayName','Shoulder plane');
+% plot(RTime,RData(:,5),'Color','g','LineWidth',LW,'DisplayName','Shoulder elevation');
+% plot(RTime,RData(:,6),'Color','b','LineWidth',LW,'DisplayName','Shoulder elevation');
+% xlabel('Time [s]')
+% ylabel('Angle [deg^o]')
+
+
+
+
+
+
+
+
 
 %% Abduction Adduction
 Row = Data(:,10)>=200;
@@ -648,3 +735,7 @@ ylabel('Angle [deg^o]')
 
 lgd = legend([L,R],'FontSize',15)
 lgd.Orientation = 'horizontal'
+
+
+
+
